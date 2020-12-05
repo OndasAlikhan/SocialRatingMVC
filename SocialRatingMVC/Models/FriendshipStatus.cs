da75174f95153a6605c7c2227a4ff0658c36a0d0
alihan.ondas <alihan.ondas@gmail.com>
2020-12-05 15:41:36 +0600

    init

---
+++ SocialRatingMVC/Models/FriendshipStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialRatingMVC.Models
+{
+    public class FriendshipStatus
+    {
+        private readonly static int STATUS_REQUESTED = 0;
+        private readonly static int STATUS_ADDED = 1;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
---
+++ SocialRatingMVC/Models/UserFriend.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialRatingMVC.Models
+{
+    public class UserFriend
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int FriendId { get; set; }
+        public int FriendshipStatusId { get; set; }
+
+        public IdentityUser User { get; set; }
+        public IdentityUser Friend { get; set; }
+        public FriendshipStatus FriendshipStatus { get; set; }
+        
+    }
+}
